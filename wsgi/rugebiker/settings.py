bfd0a5b9c62ec2f59bef60ba3a5df08fc07d2db7
Ruben Guerra Marin <rguerra.marin@gmail.com>
2013-03-30 12:48:23 -0700

    Removed the admin link on the pages

--- wsgi/rugebiker/settings.py
+++ wsgi/rugebiker/settings.py
@@ -150,8 +150,8 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
-    'blog',
     'taggit',
+    'blog',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
