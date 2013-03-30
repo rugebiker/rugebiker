498373532813016977ef43ba12973eb76ffbf6a5
Ruben Guerra Marin <rguerra.marin@gmail.com>
2013-03-29 22:31:45 -0700

    First release with just the info page

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='rugebiker',
       version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
-      url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
+      description='My own personal blog.',
+      author='Ruben Guerra Marin',
+      author_email='rguerra.marin@gmail.com',
+      url='http://www.rugebiker.com',
+      install_requires=['Django>=1.3'],
      )
