7a0e561faa036d1e2d21beef8050310c7974afd6
Ruben Guerra Marin <rguerra.marin@gmail.com>
2014-04-20 01:31:40 +0200

    Fixed manage.py to run on django 1.6

--- wsgi/rugebiker/settings.py
+++ wsgi/rugebiker/settings.py
@@ -155,7 +155,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'rugebiker.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
