8dd56a74139cf9a06c5ccd159ce715ff85a63c12
Ruben Guerra Marin <rguerra.marin@gmail.com>
2015-02-04 16:24:57 +0100

    Fixed static settings for local testing

--- rugebiker/settings.py
+++ rugebiker/settings.py
@@ -71,7 +71,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+# STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -83,12 +83,12 @@ STATIC_URL = '/static/'
 ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Additional locations of static files
-#STATICFILES_DIRS = (
+STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-#    os.path.join(PROJECT_DIR, 'static'),
-#)
+    os.path.join(PROJECT_DIR, 'static'),
+)
 
 # Make a dictionary of default keys
 default_keys = server.SERVER_KEYS 
